972068bc514ded306661a9852ccf92062ee32a54
webster <michael@webster.fyi>
2026-01-26 14:36:12 -0500

    Release v1.0.1 - Fix IDE compatibility
    - Updated IDE compatibility to support IntelliJ IDEA 2024.3+ (build 253+)
    - Fixed Taskfile YAML syntax errors with colons in echo statements
    - Updated CHANGELOG for v1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.circleci"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
@@ -52,7 +52,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("241.*")
+        untilBuild.set("253.*")
     }
 
     signPlugin {
